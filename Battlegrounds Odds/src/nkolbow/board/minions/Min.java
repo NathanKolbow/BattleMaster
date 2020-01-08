36d3f3ccc995c1445a8a7a5abe7d7d7e1e4e4999
Nathan Kolbow <nkolbow@wisc.edu>
2020-01-07 22:38:09 -0600

    added hyena token

---
+++ Battlegrounds Odds/src/nkolbow/board/minions/Min.java
@@ -0,0 +1,26 @@
+package nkolbow.board.minions;
+
+public enum Min {
+	// Curator
+	Curators_Amalgom,
+	// Tokens
+	Rover_Token, Imp, Rat, Micro_Bot, Joey_Bot, Damaged_Golem, Big_Bad_Wolf, Spider, Finkle_Einhorn, Dino_Token, Thresh_Token, Ironhide_Token, Hyena,
+	
+	// Tier 1
+	Righteous_Protector,Selfless_Hero, Wrath_Weaver, Alleycat, Dire_Wolf_Alpha, Voidwalker, Vulgar_Homunculus, Mecharoo, Micro_Machine, Murloc_Tidecaller, Murloc_Tidehunter, Rockpool_Hunter,
+	
+	// Tier 2
+	Spawn_of_NZoth, Kindly_Grandmother, Mounted_Raptor, Rat_Pack, Scavenging_Hyena, Nathrezim_Overseer, Pogo_Hopper, Annoy_o_Tron, Harvest_Golem, Kaboom_Bot, Metaltooth_Leaper, Shielded_Minibot, Zoobot, Murloc_Warleader, Old_Murk_Eye, Coldlight_Seer, Nightmare_Amalgam,
+	
+	// Tier 3
+	Crowd_Favorite, Crystalweaver, Houndmaster, Khadgar, Pack_Leader, Phalanx_Commander, Shifter_Zerus, Soul_Juggler, Tortollan_Shellraiser, Infested_Wolf, Imp_Gang_Boss, Floating_Watcher, Cobalt_Guardian, Piloted_Shredder, Psych_o_Tron, Replicating_Menace, Screwjank_Clunker,
+	
+	// Tier 4
+	Defender_of_Argus, Bolvar_Fireblood, Festeroot_Hulk, Menagerie_Magician, Virmen_Sensei, Cave_Hydra, The_Beast, Siegebreaker, Iron_Sensei, Annoy_o_Module, Security_Rover, Piloted_Sky_Golem, Toxfin, Primalfin_Lookout,
+	
+	// Tier 5
+	Baron_Rivendare, Brann_Bronzebeard, Lightfang_Enforcer, Strongshell_Scavenger, The_Boogeymonster, Goldrinn_the_Great_Wolf, Ironhide_Direhorn, Sated_Threshadon, Savannah_Highmane, Annihilan_Battlemaster, MalGanis, Voidlord, Mechano_Egg, Junkbot, King_Bagurgle,
+	
+	// Tier 6
+	Kangors_Apprentice, Zapp_Slywick, Gentle_Megasaur, Ghastcoiler, Maexxna, Mama_Bear, Foe_Reaper_4000, Sneeds_Old_Shredder
+}
